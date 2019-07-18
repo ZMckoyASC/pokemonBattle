277074e670f30ed254319f0be303628733930da9
ZMckoyASC <52672969+ZMckoyASC@users.noreply.github.com>
2019-07-18 09:49:47 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup(){
+    createCanvas(1000,1000);
+    
+}
+
+function draw(){
+    rect(30, 20, 55, 55);
+}
+
+
+function pikachu(){
+    rect(30, 20, 55, 55);
+    const health = 100
+}
+
+function someshit(){
+    
+    const health = 100
+}
